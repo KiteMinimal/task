9e1ff3e5c7ea44fb37372272e2be1d7e97139f93
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-05-24 08:18:45 +0530

    changes in style folder

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import '../styles/Login.css';
 import RegisterModal from '../components/RegisterModal';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
